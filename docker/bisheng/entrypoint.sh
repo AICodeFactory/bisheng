8e6aafb9c0752df8410cb4f77e12aa5a16c5b659
athena_labs <liuyinhou3@163.com>
2026-02-27 09:45:33 +0800

    修改

--- docker/bisheng/entrypoint.sh
+++ docker/bisheng/entrypoint.sh
@@ -7,12 +7,12 @@ start_mode=${1:-api}
 
 start_knowledge(){
   # 知识库解析的celery worker
-    celery -A bisheng.worker.main worker -l info -c 20 -P threads -Q knowledge_celery -n knowledge@%h
+    celery -A bisheng.worker.main worker -l info -c 5 -P threads -Q knowledge_celery -n knowledge@%h
 }
 
 start_workflow(){
   # 工作流相关的celery worker
-    celery -A bisheng.worker.main worker -l info -c 100 -P threads -Q workflow_celery -n workflow@%h
+    celery -A bisheng.worker.main worker -l info -c 25 -P threads -Q workflow_celery -n workflow@%h
 }
 
 start_beat(){
@@ -22,11 +22,11 @@ start_beat(){
 
 start_linsight(){
   # 灵思后台任务worker
-    python bisheng/linsight/worker.py --worker_num 4 --max_concurrency 5
+    python bisheng/linsight/worker.py --worker_num 1 --max_concurrency 2
 }
 start_default(){
     # 默认其他任务的执行worker，目前是定时统计埋点数据
-    celery -A bisheng.worker.main worker -l info -c 100 -P threads -Q celery -n celery@%h
+    celery -A bisheng.worker.main worker -l info -c 25 -P threads -Q celery -n celery@%h
 }
 
 if [ "$start_mode" = "api" ]; then
