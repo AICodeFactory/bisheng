12f9a935d2fd5b3758f58c3bd25434a856ad44b8
athena_labs <liuyinhou3@163.com>
2026-04-27 15:33:47 +0800

    降低配置

--- docker/bisheng/entrypoint.sh
+++ docker/bisheng/entrypoint.sh
@@ -7,12 +7,12 @@ start_mode=${1:-api}
 
 start_knowledge(){
   # 知识库解析的celery worker
-    celery -A bisheng.worker.main worker -l info -c 20 -P threads -Q knowledge_celery -n knowledge@%h
+    celery -A bisheng.worker.main worker -l info -c 5 -P threads -Q knowledge_celery -n knowledge@%h
 }
 
 start_workflow(){
   # 工作流相关的celery worker
-    celery -A bisheng.worker.main worker -l info -c 100 -P threads -Q workflow_celery -n workflow@%h
+    celery -A bisheng.worker.main worker -l info -c 25 -P threads -Q workflow_celery -n workflow@%h
 }
 
 start_beat(){
@@ -22,16 +22,16 @@ start_beat(){
 
 start_linsight(){
   # 灵思后台任务worker
-    python bisheng/linsight/worker.py --worker_num 4 --max_concurrency 5
+    python bisheng/linsight/worker.py --worker_num 1 --max_concurrency 2
 }
 start_default(){
     # 默认其他任务的执行worker，目前是定时统计埋点数据
-    celery -A bisheng.worker.main worker -l info -c 100 -P threads -Q celery -n celery@%h
+    celery -A bisheng.worker.main worker -l info -c 25 -P threads -Q celery -n celery@%h
 }
 
 if [ "$start_mode" = "api" ]; then
     echo "Starting API server..."
-    uvicorn bisheng.main:app --host 0.0.0.0 --port 7860 --no-access-log --workers 8
+    uvicorn bisheng.main:app --host 0.0.0.0 --port 7860 --no-access-log --workers 2
 elif [ "$start_mode" = "knowledge" ]; then
     echo "Starting Knowledge Celery worker..."
     start_knowledge
